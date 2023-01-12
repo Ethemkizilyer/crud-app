e6191491d1e9c63129535c7cd85e10c11e10a66a
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-12 08:01:29 +0300

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { AuthProvider } from "../context/AuthContext";
-
+import "../styles/globals.css"
 
 export default function Layout(props) {
   const { children } = props;
-  return ( <AuthProvider >
+  return ( 
     <div className="flex flex-col min-h-screen relative bg-slate-900 text-white">
-       
+       <AuthProvider >
       <Header />
       <main className="flex-1 flex flex-col p-4">{children}</main>
       <Footer />
-      
-    </div></AuthProvider>
+      </AuthProvider>
+    </div>
   );
 }
