cac5a6e4a60a72d74dcaef65906f7158aead7481
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-13 03:55:49 +0300

    feat: .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,13 @@ import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className="flex justify-center items-center gap-5 py-3">
-      <a href="#" target="_blank">
+      <a href="#">
         <FaInstagram className="duration-300 hover:opacity-30 cursor-pointer" />
       </a>
-      <a href="https://www.linkedin.com/in/ethem-kizilyer/" target="_blank">
+      <a href="https://www.linkedin.com/in/ethem-kizilyer/" >
         <FaLinkedin className="duration-300 hover:opacity-30 cursor-pointer" />
       </a>
-      <a href="https://github.com/Ethemkizilyer" target="_blank">
+      <a href="https://github.com/Ethemkizilyer">
         <FaGithub className="duration-300 hover:opacity-30 cursor-pointer" />
       </a>
     </div>
