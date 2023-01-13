f0419373e0c1de063b05de63f9a5d4ec72c44308
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-13 04:18:22 +0300

    feat: .env added

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    apiKey: process.env.NEXT_PUBLIC_APIKEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_APROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_ASTORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_AMESSAING_ID,
-    appId: process.env.NEXT_PUBLIC_AAPP_ID,
+    NEXT_PUBLIC_APIKEY: "AIzaSyBdgBklh64dL3YiTLRZomQA7ZWIRchzR_8",
+    NEXT_PUBLIC_AUTH_DOMAIN: "nextjs-2b888.firebaseapp.com",
+    NEXT_PUBLIC_APROJECT_ID: "nextjs-2b888",
+    NEXT_PUBLIC_ASTORAGE_BUCKET: "nextjs-2b888.appspot.com",
+    NEXT_PUBLIC_AMESSAING_ID: "1052151823182",
+    NEXT_PUBLIC_AAPP_ID: "1:1052151823182:web:e78cea1ffe31568f84fc1f",
   },
 };
 
