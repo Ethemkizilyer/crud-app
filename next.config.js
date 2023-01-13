43796fb652d44fa4d69c272685caee9532f54ec8
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-13 04:09:12 +0300

    feat: vercel updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    apiKey: process.env.NEXT_PUBLIC_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_APROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_ASTORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_AMESSAING_ID,
+    appId: process.env.NEXT_PUBLIC_AAPP_ID,
+  },
 };
 
 module.exports = nextConfig;
