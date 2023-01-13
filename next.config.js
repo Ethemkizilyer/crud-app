2e18d5bf57bf6534ca6235c5c76443f35889b17f
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-13 04:43:02 +0300

    .

--- next.config.js
+++ next.config.js
@@ -2,15 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  // env: {
-  //   NEXT_PUBLIC_APIKEY: "AIzaSyBdgBklh64dL3YiTLRZomQA7ZWIRchzR_8",
-  //   NEXT_PUBLIC_AUTH_DOMAIN: "nextjs-2b888.firebaseapp.com",
-  //   NEXT_PUBLIC_APROJECT_ID: "nextjs-2b888",
-  //   NEXT_PUBLIC_ASTORAGE_BUCKET: "nextjs-2b888.appspot.com",
-  //   NEXT_PUBLIC_AMESSAING_ID: "1052151823182",
-  //   NEXT_PUBLIC_AAPP_ID: "1:1052151823182:web:e78cea1ffe31568f84fc1f",
-  // },
+  }
 };
 
 module.exports = nextConfig;
